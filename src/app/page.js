f9a2625b7e249bc10df23edcd6d5de6ef11509b0
Alexander Machado <alexuandermachado@gmail.com>
2023-10-17 14:10:34 -0600

    ✅ Configuracion Inicial del proyecto

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "@nextui-org/react";
+export default function Home() {
+	return (
+		<>
+			<div>Aplicacion de Ecomerce</div>
+			<Button>Click me</Button>
+		</>
+	);
+}
---
+++ src/app/providers.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+import React from "react";
+
+export default function Providers({ children }) {
+	return <NextUIProvider>{children}</NextUIProvider>;
+}
