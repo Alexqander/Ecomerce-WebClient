8ad81c01663679d25541a038c670baa2466802cd
Rodrigo Dorantes <rodrigoivan234@gmail.com>
2023-10-26 18:19:03 -0600

    ➕ Pagina de ordenes

---
+++ src/app/dashboard/ordenes/data.js
@@ -0,0 +1,57 @@
+import React from "react";
+const columns = [
+  {name: "NOMBRE", uid: "name"},
+  {name: "DESCRIPCIÓN", uid: "role"},
+  {name: "STATUS", uid: "status"},
+  {name: "ACTIONS", uid: "actions"},
+];
+
+const users = [
+  {
+    id: 1,
+    name: "Leche",
+    role: "Leche en bote",
+    status: "active",
+    age: "29",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+    email: "tony.reichert@example.com",
+  },
+  {
+    id: 2,
+    name: "Agua",
+    role: "Agua purificada",
+    status: "paused",
+    age: "25",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
+    email: "zoey.lang@example.com",
+  },
+  {
+    id: 3,
+    name: "Botellas",
+    role: "Botellas de vino",
+    status: "active",
+    age: "22",
+    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+    email: "jane.fisher@example.com",
+  },
+  {
+    id: 4,
+    name: "Chocolate",
+    role: "Chocolate en barra",
+    status: "vacation",
+    age: "28",
+    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
+    email: "william.howard@example.com",
+  },
+  {
+    id: 5,
+    name: "Tenis",
+    role: "Sales Manager",
+    status: "active",
+    age: "24",
+    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
+    email: "kristen.cooper@example.com",
+  },
+];
+
+export {columns, users};
